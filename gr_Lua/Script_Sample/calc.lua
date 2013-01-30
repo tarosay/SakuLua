be779ef7153d1bb3f62c853fb00ce26d03cbe091
Tarosa <tarosa.y@gmail.com>
2013-01-31 03:59:04 +0900

    sample update

---
+++ gr_Lua/Script_Sample/calc.lua
@@ -0,0 +1,29 @@
+----Calc
+sys.setrun("calc.lua")
+while(true)do
+	Serial.print(">")
+	m=""
+	while(true)do
+		repeat 
+			k = Serial.read()
+		until(k~=-1)
+		if(k==10)then break end
+		if(k==8)then
+			m=m:sub(1,m:len()-1)
+		else
+			m=m..string.format("%c",k)
+		end
+		Serial.print(string.format("%c",k))
+	end
+	--Serial.println("m:len()="..m:len())
+	if(m:len()==0)then break end
+	m = m:gsub("^%s*(.-)%s*$", "%1")
+	ans = loadstring( "return {"..m.."}" )()[1]
+	if(ans~=nill)then
+		Serial.println("\n>"..ans)
+	else
+		Serial.println("")
+	end
+end
+Serial.println("\n loader.lua Running...")
+sys.setrun("loader.lua")
