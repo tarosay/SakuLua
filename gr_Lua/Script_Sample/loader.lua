cf36b65815fdcd9e08dc34c641da9aff239fb7fe
Tarosa <tarosa.y@gmail.com>
2013-02-01 02:39:15 +0900

    loader.lua Update

--- gr_Lua/Script_Sample/loader.lua
+++ gr_Lua/Script_Sample/loader.lua
@@ -1,30 +1,31 @@
 ----LuaLoader
 fm=""
+Serial.print("\nInput Lua filename>")
 while(true)do
 	repeat 
 		k = Serial.read()
-	until(k~=-1)
+	until(k>0)
 	if(k==10)then break end
 	fm=fm..string.format("%c",k)
 	Serial.print(string.format("%c",k))
 end
-fm = fm:gsub("^%s*(.-)%s*$", "%1")	--トリミング
+fm = fm:gsub("^%s*(.-)%s*$", "%1")
 Serial.println("\nFilename:["..fm.."]")
 
 fp = 0
-SD.open( fp, fm, 2 ) --新規書き込みオープン
+SD.open( fp, fm, 2 )
 SD.write( fp, "--"..fm.."\n", fm:len()+3 )
 eflg=0
 while(true)do
 	t=millis()+1000
 	repeat 
-		k = Serial.read()	--文字入力があるまで待つ
+		k = Serial.read()
 		if(t<millis())then
-			Serial.println("Contimue?(y/n)")
+			Serial.println("Close?(Enter)")
 			repeat 
-				l = Serial.read()	--文字入力があるまで待つ
-			until(l~=-1)
-			if(l==110)then
+				l = Serial.read()
+			until(l>0)
+			if(l==10)then
 				eflg = 1
 				break
 			else
@@ -32,16 +33,10 @@ while(true)do
 			end
 			t=millis()+1000
 		end
-	until(k~=-1)
+		if(k==0)then k=10 end
+	until(k>0)
 	if(eflg==1)then break end
-	
 	SD.write( fp, string.format("%c",k), 1 )
-end
-SD.close( fp ) 
-SD.open(fp, fm) 
-while(true)do
-	k=SD.read(fp)
-	if(k==-1)then break end
 	Serial.print(string.format("%c",k))
 end
 SD.close( fp ) 
