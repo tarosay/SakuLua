e65b79d71e49c43b3e02ae46041c15a64f98a4a1
Tarosa <tarosa.y@gmail.com>
2013-02-01 05:06:17 +0900

    linerun.lua Update

---
+++ gr_Lua/Script_Sample/linerun.lua
@@ -0,0 +1,27 @@
+----One Line Run
+Serial.print("\nOne Line Run>")
+m=""
+while(true)do
+	repeat 
+		k = Serial.read()
+	until(k>0)
+	if(k==10)then break end
+	if(k==8)then
+		m=m:sub(1,m:len()-1)
+	else
+		m=m..string.format("%c",k)
+	end
+	Serial.print(string.format("%c",k))
+end
+Serial.print("\n")
+if(m:len()~=0)then
+	m = m:gsub("^%s*(.-)%s*$", "%1")
+	SD.open( 0, "lineruns.lua", 2 )
+	SD.write(fp, m, m:len())
+	m=";if(a~=nil)then Serial.println(a) end;"
+	SD.write(fp, m, m:len())
+	m="sys.setrun('linerun.lua')"
+	SD.write(fp, m, m:len())
+	SD.close( fp ) 
+	sys.setrun("lineruns.lua")
+end
