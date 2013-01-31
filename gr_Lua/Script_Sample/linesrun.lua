53ee70b60fc3e8fa72e7872d7cd232018218ab11
Tarosa <tarosa.y@gmail.com>
2013-02-01 05:33:40 +0900

    linesrun.lua Update

---
+++ gr_Lua/Script_Sample/linesrun.lua
@@ -0,0 +1,28 @@
+----One Line Run
+Serial.println("Input end with [Ctrl-z]")
+m=""
+while(true)do
+	repeat 
+		k = Serial.read()
+	until(k>0)
+	if(k==26)then break end
+	if(k==8)then
+		m=m:sub(1,m:len()-1)
+	else
+		m=m..string.format("%c",k)
+	end
+	Serial.print(string.format("%c",k))
+end
+Serial.print("\n")
+if(m:len()==0)then
+	Serial.println("\n loader.lua Running...")
+	sys.setrun("loader.lua")
+else
+	m = m:gsub("^%s*(.-)%s*$", "%1")
+	SD.open( 0, "linesrus.lua", 2 )
+	SD.write(fp, m, m:len())
+	m=";if(a~=nil)then Serial.println(a) end;"
+	SD.write(fp, m, m:len())
+	SD.close( fp ) 
+	sys.setrun("linesrus.lua")
+end
